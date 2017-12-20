20d466b2e3eaa3311c134c69eae2026762b3e5f1
Roman Averin <rastler@gmail.com>
2017-12-20 23:40:45 +0300

    job02

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,9 +1,21 @@
 import path from 'path';
 import genDiff from '../src';
 
-test('Testing genDiff with fixtures', () => {
+test('Testing genDiff with JSON', () => {
   const testFile1 = path.join(__dirname, '__fixtures__/befor.json');
   const testFile2 = path.join(__dirname, '__fixtures__/after.json');
-  const array = 'host: hexlet.io\n- timeout: 50\n+ timeout: 20\n- proxy: 123.234.53.22\n+ verbose: true';
+  const array = '{\n host: hexlet.io\n- timeout: 50\n+ timeout: 20\n- proxy: 123.234.53.22\n+ verbose: true\n}';
+  expect(genDiff(testFile1, testFile2)).toEqual(array);
+});
+test('Testing genDiff with YML', () => {
+  const testFile1 = path.join(__dirname, '__fixtures__/befor.yml');
+  const testFile2 = path.join(__dirname, '__fixtures__/after.yml');
+  const array = '{\n host: hexlet.io\n- timeout: 50\n+ timeout: 20\n- proxy: 123.234.53.22\n+ verbose: true\n}';
+  expect(genDiff(testFile1, testFile2)).toEqual(array);
+});
+test('Testing genDiff with JSON and YML', () => {
+  const testFile1 = path.join(__dirname, '__fixtures__/befor.json');
+  const testFile2 = path.join(__dirname, '__fixtures__/after.yml');
+  const array = '{\n host: hexlet.io\n- timeout: 50\n+ timeout: 20\n- proxy: 123.234.53.22\n+ verbose: true\n}';
   expect(genDiff(testFile1, testFile2)).toEqual(array);
 });
--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 import { readFileSync } from 'fs';
+import path from 'path';
 import _ from 'lodash';
+import yaml from 'js-yaml';
 
+const selectParseFn = {
+  '.json': JSON.parse,
+  '.yml': yaml.safeLoad,
+};
+const process = (pointer, fileType) => {
+  const parseFn = selectParseFn[fileType];
+  return parseFn(pointer);
+};
 const compare = (obj1, obj2) => {
   const keys = Object.keys({ ...obj1, ...obj2 });
   return _.reduce(keys, (acc, key) => {
@@ -20,7 +30,9 @@ const compare = (obj1, obj2) => {
   }, []);
 };
 export default (pathToFile1, pathToFile2) => {
-  const obj1 = JSON.parse(readFileSync(pathToFile1));
-  const obj2 = JSON.parse(readFileSync(pathToFile2));
-  return compare(obj1, obj2).join('\n');
+  const pointerFile1 = readFileSync(pathToFile1);
+  const pointerFile2 = readFileSync(pathToFile2);
+  const obj1 = process(pointerFile1, path.extname(pathToFile1));
+  const obj2 = process(pointerFile2, path.extname(pathToFile2));
+  return `{\n ${compare(obj1, obj2).join('\n')}\n}`;
 };
