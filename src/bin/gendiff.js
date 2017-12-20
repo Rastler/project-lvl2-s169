5a50f514e7d63b33078ed2636474b93cc2f00a80
Roman Averin <rastler@gmail.com>
2017-12-21 00:02:35 +0300

    fix

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -4,7 +4,7 @@ import program from 'commander';
 import gendiff from '../';
 import version from '../../package.json';
 
-console.log(version);
+
 program
   .version(version)
   .description('Compares two configuration files and shows a difference.')
